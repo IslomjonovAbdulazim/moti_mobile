3299f375ea581c1c3735c02468e79e60f3cab4ad
abdulazim <islomjonov.abdulazim.27@gmail.com>
2022-09-13 19:02:20 +0500

    auth bo'ldi, lekin error bermidi user ga

---
+++ lib/services/apis.dart
@@ -0,0 +1,16 @@
+class Apis {
+  Apis._();
+
+  static Apis instance = Apis._();
+
+  ///base url
+  String baseUrl = "78.24.219.215:8091";
+
+  ///auth
+  String registerAdmin = "/api/v1/auth/add_admin";
+  String loginAdmin = "/api/v1/auth/login_admin";
+  String loginUser = "/api/v1/auth/login_user";
+  String registerUser = "/api/v1/auth/register_user";
+  String sendActivationCode = "/api/v1/auth/send_code";
+  String verifyUser = "/api/v1/auth/verify_code";
+}
---
+++ lib/services/network_service.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:moti/services/apis.dart';
+
+class NetworkService {
+  final apis = Apis.instance;
+  NetworkService._();
+
+  static final instance = NetworkService._();
+
+  Map<String, String> get headers {
+    return {
+      'Content-type': 'application/json; charset=UTF-8',
+    };
+  }
+
+  Future<String?> GET(String baseUrl, String api,
+      {Map<String, String>? params}) async {
+    Uri url = Uri.http(baseUrl, api, params);
+    print(url);
+    Response response = await get(url, headers: headers);
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    }
+
+    return null;
+  }
+
+  Future<Response?> POST(String api, Map<String, dynamic>? body, {Map<String, dynamic>? param}) async {
+    // print(param);
+    String a = (param != null? "?$param": "");
+    print(api+a);
+    Uri url = Uri.http(apis.baseUrl, api,param);
+    print('url: $url');
+    Response response =
+        await post(url, headers: headers, body: jsonEncode(body));
+print(response.statusCode);
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response;
+    }
+
+    return null;
+  }
+
+  Future<String?> PUT(
+      String baseUrl, String api, Map<String, dynamic>? body) async {
+    Uri url = Uri.https(baseUrl, api);
+    Response response =
+        await put(url, headers: headers, body: jsonEncode(body));
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    }
+
+    return null;
+  }
+
+  Future<String?> DELETE(String baseUrl, String api,
+      {Map<String, String>? params}) async {
+    Uri url = Uri.https(baseUrl, api, params);
+    Response response = await delete(url, headers: headers);
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    }
+
+    return null;
+  }
+}
